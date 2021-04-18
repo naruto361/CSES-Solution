847506b6c0952840b65494be65f67ce3db1e447e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-18 20:21:08 +0530

    Add files via upload

---
+++ movie_festival.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool sortbysec(const pair<int,int> &a, 
+              const pair<int,int> &b) 
+{ 
+    return (a.second < b.second); 
+}
+int main()
+{
+    vector<pair<int,int>> v;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        v.push_back({x,y});
+    }
+    sort(v.begin(),v.end(),sortbysec);
+    int ans=1;
+    int end=v[0].second;
+    for(int i=1;i<v.size();i++)
+    {
+        if(v[i].first>=end)
+        {
+            ans++;
+            end=v[i].second;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
